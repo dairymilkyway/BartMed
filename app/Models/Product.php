b9e046f31442eedcc5f743c55fb5e77b16f3b657
JoshNotheGreat <Joshziemenntan998@gmail.com>
2024-06-28 12:59:48 +0800

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\belongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\Brand;
 
 class Product extends Model
 {
     use HasFactory;
+    protected $table = 'products';
+    protected $primaryKey = 'id';
+    public $timestamps = true; 
+    protected $fillable = [
+        'id',
+        'brand_id'.
+        'product_name',
+        'description',
+        'price',
+        'stocks',
+        'category',
+        'img_path',
+    ];
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class, 'brand_id', 'id');
+    }
+
 }
