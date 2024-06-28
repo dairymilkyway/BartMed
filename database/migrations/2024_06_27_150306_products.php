269f6e94b7344f718b3a488f567e4fde9f7300e1
JoshNotheGreat <Joshziemenntan998@gmail.com>
2024-06-28 12:23:24 +0800

    Update 2024_06_27_150306_products.php

--- database/migrations/2024_06_27_150306_products.php
+++ database/migrations/2024_06_27_150306_products.php
@@ -13,12 +13,12 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('category_id');
             $table->unsignedBigInteger('brand_id');
             $table->string('product_name');
             $table->string('description');
             $table->double('price');
             $table->integer('stocks');
+            $table->string('category');
             $table->string('img_path');
             $table->timestamps();
 
