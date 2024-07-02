861bd2a9627febaa3cd2f53fa054072707bfeefa
JoshNotheGreat <Joshziemenntan998@gmail.com>
2024-07-02 14:08:38 +0800

    Update Supplier.php

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Supplier extends Model
 {
     use HasFactory;
+    protected $table = 'suppliers';
+    protected $primaryKey = 'id';
+    public $timestamps = true; 
+    protected $fillable = [
+        'id',
+        'supplier_name',
+        'img_path',
+    ];
 }
