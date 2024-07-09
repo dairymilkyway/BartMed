9e7ee265e8304ca89d25464e0777d8e9487c1500
JoshNotheGreat <Joshziemenntan998@gmail.com>
2024-07-09 13:43:02 +0800

    Update Customer.php

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\User;
 class Customer extends Model
 {
     use HasFactory;
+    protected $table = 'brands';
+    protected $primaryKey = 'id';
+    public $timestamps = true; 
+    protected $fillable = [
+        'user_id',
+        'name',
+        'address',
+        'number',
+        'img_path'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
 }
