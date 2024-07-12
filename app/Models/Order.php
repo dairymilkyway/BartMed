c8d5d653104d4b6faf8d44e8ba128b669816f6b4
JoshNotheGreat <Joshziemenntan998@gmail.com>
2024-07-12 20:41:42 +0800

    Update Order.php

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Customer;
 
 class Order extends Model
 {
     use HasFactory;
+    public $timestamps = true; 
+    protected $fillable = [
+        'customer_id',
+        'order_status',
+        'courier',
+        'payment_method',
+    ];
+
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class,'customer_id', 'id');
+    }
 }
