7722aa39ba72dfa81840f356c480a2c264bb0dbe
JoshNotheGreat <Joshziemenntan998@gmail.com>
2024-07-13 19:47:13 +0800

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -66,6 +66,7 @@
     <script src="{{ asset('js/product.js') }}"></script>
     <script src="{{ asset('js/supplier.js') }}"></script>
     <script src="{{ asset('js/customer.js') }}"></script>
+    <script src="{{ asset('js/order.js') }}"></script>
     @vite('resources/css/app.css')
 </head>
 
