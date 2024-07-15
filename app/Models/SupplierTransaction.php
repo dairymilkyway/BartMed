964facfc06a30e26a5be457871e3598ad3ff7d5b
JoshNotheGreat <Joshziemenntan998@gmail.com>
2024-07-15 16:59:17 +0800

    Update SupplierTransaction.php

--- app/Models/SupplierTransaction.php
+++ app/Models/SupplierTransaction.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\Product;
+use App\Models\Supplier;
 class SupplierTransaction extends Model
 {
     use HasFactory;
+    protected $table = 'supplier_transaction';
+    protected $primaryKey = 'id';
+    public $timestamps = true; 
+    protected $fillable = [
+        'supplier_id', 
+        'product_id',
+        'img_path',
+        'quantity'
+    ];
+
+    public function supplier(): BelongsTo
+    {
+        return $this->belongsTo(Supplier::class, 'supplier_id', 'id');
+    }
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class, 'product_id', 'id');
+    }
 }
