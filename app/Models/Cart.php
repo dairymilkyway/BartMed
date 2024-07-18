8c2dd96ef84290e9435c479f32b3ba95f60098d3
JoshNotheGreat <Joshziemenntan998@gmail.com>
2024-07-18 21:57:43 +0800

    Update Cart.php

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -4,10 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\belongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\Customer;
+use App\Models\Product;
 class Cart extends Model
 {
     use HasFactory;
     protected $table = 'carts';
     public $timestamps = true;
+    protected $fillable = [
+        'customer_id',
+        'product_id',
+        'quantity'
+    ];
+
+    // Define the relationship with the Customer model
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    // Define the relationship with the Product model
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
