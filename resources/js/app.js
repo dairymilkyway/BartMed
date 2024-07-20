2f0dbf9ab123760a541dc04f03e5d26fc3208f2c
Gwyn <skythekid211@gmail.com>
2024-07-20 15:41:14 +0800

    add feature

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
 import './bootstrap';
+
+import search from './search.js'; // Import the search module
+import 'instantsearch.css/themes/satellite.css'; // Import the InstantSearch CSS
+
+search.start(); // Initialize the search functionality
