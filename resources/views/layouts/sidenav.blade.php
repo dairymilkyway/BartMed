34c42302e94dbc8ba20aa18a2ecf2ad9888e1e7f
JoshNotheGreat <Joshziemenntan998@gmail.com>
2024-07-21 19:13:30 +0800

    Update sidenav.blade.php

--- resources/views/layouts/sidenav.blade.php
+++ resources/views/layouts/sidenav.blade.php
@@ -102,10 +102,11 @@
 
         <li>
           <a
-            href="#"
+            href="{{route('brand.chart')}}"
             class="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
           >
-            Supplier Bought
+            
+          Brand Solds
           </a>
         </li>
       </ul>
