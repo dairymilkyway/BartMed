12bfc1c68bff33297b7cccb6b690afa8e31bcb3d
JoshNotheGreat <Joshziemenntan998@gmail.com>
2024-07-28 00:02:13 +0800

    Update chart.blade.php

--- resources/views/brand/chart.blade.php
+++ resources/views/brand/chart.blade.php
@@ -21,12 +21,23 @@
                             var brandNames = [];
                             var totalSold = [];
                             var brandDetails = {};
+                            var backgroundColors = [];
+                            var borderColors = [];
+
+                            function getRandomColor(alpha) {
+                                var r = Math.floor(Math.random() * 255);
+                                var g = Math.floor(Math.random() * 255);
+                                var b = Math.floor(Math.random() * 255);
+                                return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+                            }
 
                             data.forEach(function(brand) {
                                 brandNames.push(brand.brand_name);
                                 totalSold.push(brand.total_sold);
-
                                 brandDetails[brand.brand_name] = brand.total_sold;
+
+                                backgroundColors.push(getRandomColor(0.2));
+                                borderColors.push(getRandomColor(1));
                             });
 
                             var ctx = document.getElementById('brandSalesChart').getContext('2d');
@@ -36,22 +47,8 @@
                                     labels: brandNames,
                                     datasets: [{
                                         data: totalSold,
-                                        backgroundColor: [
-                                            'rgba(255, 99, 132, 0.2)',
-                                            'rgba(54, 162, 235, 0.2)',
-                                            'rgba(255, 206, 86, 0.2)',
-                                            'rgba(75, 192, 192, 0.2)',
-                                            'rgba(153, 102, 255, 0.2)',
-                                            'rgba(255, 159, 64, 0.2)'
-                                        ],
-                                        borderColor: [
-                                            'rgba(255, 99, 132, 1)',
-                                            'rgba(54, 162, 235, 1)',
-                                            'rgba(255, 206, 86, 1)',
-                                            'rgba(75, 192, 192, 1)',
-                                            'rgba(153, 102, 255, 1)',
-                                            'rgba(255, 159, 64, 1)'
-                                        ],
+                                        backgroundColor: backgroundColors,
+                                        borderColor: borderColors,
                                         borderWidth: 1
                                     }]
                                 },
