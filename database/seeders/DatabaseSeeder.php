f90409ca1e4bda68fdbf34cfa94c03ae1d0d4fc5
Gwyn <skythekid211@gmail.com>
2024-07-28 03:50:49 +0800

    db seed nalang

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Customer;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-    }
+            $this->call(UserSeeder::class);
+            $this->call(AdminSeeder::class);
+            $this->call(BrandsSeeder::class);
+            $this->call(ProductsSeeder::class);
+            $this->call(CustomerSeeder::class);
+            $this->call(OrdersSeeder::class);
+            $this->call(OrdersProductSeeder::class);
+        }
+    
 }
